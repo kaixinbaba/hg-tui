526489590ca86257119340aa24a38b749ef866d8
junjie.xun <junjie.xun@weimob.com>
2022-05-06 22:31:02 +0800

    fix windows HOME and release.yml

--- src/config.rs
+++ src/config.rs
@@ -8,7 +8,12 @@ pub struct Config {
 
 impl From<crate::cli::Args> for Config {
     fn from(args: crate::cli::Args) -> Self {
-        let config_path = args.path.unwrap_or_else(|| env!("HOME").to_string());
+        #[cfg(not(target_os = "windows"))]
+        let home = env!("HOME").to_string();
+        #[cfg(target_os = "windows")]
+        let home = env!("HOMEPATH").to_string();
+
+        let config_path = args.path.unwrap_or(home);
         Config {
             moyu: args.moyu,
             config_path,
