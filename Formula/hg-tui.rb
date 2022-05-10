d7e5ed39abf7aea6a45e1da8908f2aafd2888f86
junjie.xun <junjie.xun@weimob.com>
2022-05-10 21:49:43 +0800

    test homebrew

--- Formula/hg-tui.rb
+++ Formula/hg-tui.rb
@@ -1,4 +1,4 @@
-class HgTui < Formula
+class hg-tui < Formula
   version "0.1.2"
   desc "A TUI application to view www.hellogithub.com"
   homepage "https://github.com/kaixinbaba/hg-tui"
@@ -6,17 +6,17 @@ class HgTui < Formula
   disable! because: "it is now in homebrew core. Please reinstall it as follows:\nbrew untap kaixinbaba/hg-tui\nbrew install hg-tui\n"
 
   if OS.mac?
-      url "https://github.com/kaixinbaba/hg-tui/releases/download/#{version}/hgtui_#{version}_macOS.tar.gz"
+      url "https://github.com/kaixinbaba/hg-tui/releases/download/#{version}/hg-tui_#{version}_macOS.tar.gz"
       sha256 "9d0fa738d8c421761536b3c4044a8a29728fc68cd60873fb552720e2d697b998"
   elsif OS.linux?
-      url "https://github.com/kaixinbaba/hg-tui/releases/download/#{version}/hgtui_#{version}_linux.tar.gz"
+      url "https://github.com/kaixinbaba/hg-tui/releases/download/#{version}/hg-tui_#{version}_linux.tar.gz"
       sha256 "6dd535c3902878e9de94a77cbe5439cfd6f11a1703929c0f07a6ee80f6e8f724"
   end
 
-  conflicts_with "hgtui"
+  conflicts_with "hg-tui"
 
   def install
-    bin.install "hgtui"
-    ohai "Just use hgtui"
+    bin.install "hg-tui"
+    ohai "Just use hg-tui"
   end
 end
