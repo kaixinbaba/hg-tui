bd407a0921a8f9fb4d021024eff589b3613ff63b
Jeff Xun <452914639@qq.com>
2022-05-10 22:18:35 -0400

    hg-tui 0.1.3
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/hg-tui.rb
+++ Formula/hg-tui.rb
@@ -1,8 +1,8 @@
 class HgTui < Formula
   desc "使用 TUI 界面去浏览 HelloGitHub 网站"
   homepage "https://github.com/kaixinbaba/hg-tui"
-  url "https://github.com/kaixinbaba/hg-tui/archive/0.1.2.tar.gz"
-  sha256 "1e3eed1ad8f18d7649285e9874c1d441730884e1701af536f573dbae4a2f6afd"
+  url "https://github.com/kaixinbaba/hg-tui/archive/0.1.3.tar.gz"
+  sha256 "23cbd2b92010c66849c2c8832e0bb2837697c19abefbd0fb8e19580fc1f33da6"
   license "GPL-3.0"
 
   def install
